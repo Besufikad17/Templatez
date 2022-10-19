51ac41746c66097c0e3f8caa3eb9cfd8d6013391
Besufikad17 <besufikadtilahun1994@gmail.com>
2022-10-19 22:01:30 +0300

    first version completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from zipfile import ZipFile
 from halo import Halo
 
 
@@ -31,26 +32,51 @@ TEMPLATES = [
         "github": "https://github.com/Besufikad17/express-js-mongo-template",
         "autour": "Besufikad"
     }
-    
-
-
 ]
+
+def install(url, fpath, github):
+    halo = Halo("Downloading template..", spinner='dots')
+    halo.start()
+    response = requests.get(url)
+    open("template.zip", "wb").write(response.content)
+    halo.stop()
+
+    halo.clear()
+    halo.start("Extracting files..")
+    with ZipFile("template.zip", 'r') as zip_ref:
+        zip_ref.extractall(fpath)
+    halo.stop()
+
+    halo.clear()
+    halo.start("Cleaning files..")
+    
+    os.remove("template.zip")
+    halo.stop()
+    print(f"Done, make sure you follow instructions listed here '{github}' and star the project 😜")
 
 def print_menu():
     print("\t Welcome to Automata 🤖")
     print("1. Create react-vite template")
     print("2. Create express-ts-mongo")
-    print("3. Exit")
+    print("3. Create express-js-mongo")
+    print("4. Exit")
     choice = int(input('-> '))
     return choice
 
 
 def main():
+    cwd = os.getcwd()
     while True:
         ch = print_menu()
+        if ch != 4 and ch > 0:
+            url = TEMPLATES[ch - 1]["url"]
+            github_url = TEMPLATES[ch - 1]["github"]
+            install(url, cwd, github_url)
+            break
+        elif ch == 4:
+            print("Oyasumi")
+            break
+        else:
+            print("Invalid option!! please try again.")
 
-
-
-# response = requests.get(URL)
-# open("template.zip", "wb").write(response.content)
-
+main()
