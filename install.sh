9568fa017169215ee8746ed8a116ee23ab13a638
besufikad17 <besufikadtilahun1994@gmail.com>
2023-08-03 20:00:16 +0300

    bug fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/Besufikad17/Templatez.git
 cd Templatez
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 sudo chmod +x templatez.py
 sudo cp ./templatez.py /usr/local/bin
