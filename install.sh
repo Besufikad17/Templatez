24a0a3c51eed2aeb91104c9a172ad22797d233ec
besufikad17 <besufikadtilahun1994@gmail.com>
2023-08-03 20:59:50 +0300

    bug fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/Besufikad17/Templatez.git
-cd Templatez
-sudo pip3 install -r requirements.txt
-sudo chmod +x templatez.py
+git clone https://github.com/Besufikad17/Templatez.git &
+cd Templatez &
+sudo pip3 install -r requirements.txt &
+sudo chmod +x templatez.py &
 python3 templatez.py
